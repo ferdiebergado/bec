fb149c50caf808cf05597a2b161043d54487f9b8
Ferdie Bergado <ferdiebergado@gmail.com>
2023-11-05 11:38:58 +0800

    Used absolute path

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import serverless from 'serverless-http';
-import { router, staticHandler } from '../../src/app';
+import { router } from '../../src/app';
+import path from 'path';
 // import config from '../../src/config';
-// import path from 'path';
 
 const app = express();
 // const { paths } = config;
 
-app.use(express.static('../../public'));
+app.use(express.static(path.join(__dirname, '../../public')));
 app.use('/.netlify/functions/api', router);
 
 app.get('/', (_req, res) => {
   //   res.sendFile(path.join(paths.public, 'index.html'));
-  res.sendFile('../../public/index.html');
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 // app.use(errorHandler);
 
