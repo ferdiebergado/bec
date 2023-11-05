534da8296aeb24727694bdba88db8b55249e3b69
Ferdie Bergado <ferdiebergado@gmail.com>
2023-11-05 11:28:05 +0800

    Changed to send from /public directly

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,18 +1,19 @@
 import express from 'express';
 import serverless from 'serverless-http';
-import { router, staticHandler } from '../../src/server';
-import config from '../../src/config';
-import path from 'path';
+import { router, staticHandler } from '../../src/app';
+// import config from '../../src/config';
+// import path from 'path';
 
 const app = express();
-const { paths } = config;
+// const { paths } = config;
 
 app.use(staticHandler);
+app.use('/.netlify/functions/api', router);
 
 app.get('/', (_req, res) => {
-  res.sendFile(path.join(paths.public, 'index.html'));
+  //   res.sendFile(path.join(paths.public, 'index.html'));
+  res.sendFile('/public/index.html');
 });
 // app.use(errorHandler);
-app.use('/.netlify/functions/api', router);
 
 export const handler = serverless(app);
