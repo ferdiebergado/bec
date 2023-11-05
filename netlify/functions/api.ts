d0e68d42c7c8a7033f0c396232bdf7d085984510
Ferdie Bergado <ferdiebergado@gmail.com>
2023-11-05 13:16:51 +0800

    Used /api routh

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -7,7 +7,7 @@ const app = express();
 // const { paths } = config;
 
 // app.use(express.static(path.join(__dirname, '../../public')));
-app.use('/.netlify/functions/api', router);
+app.use('/api/', router);
 
 // app.get('/', (_req, res) => {
 //   //   res.sendFile(path.join(paths.public, 'index.html'));
